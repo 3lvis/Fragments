4f042031dd1a87e67bb65ccc248cea01f460761e
Elvis Nuñez <elvisnunez@me.com>
2015-04-28 20:57:13 +0200

    Add empty Demo

---
+++ Pod/Demo/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window?.rootViewController = Tailor()
+    window?.makeKeyAndVisible()
+
+    return true
+  }
+}
+
---
+++ Source/Tailor.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class Tailor : UIViewController {
+  override func viewDidLoad() {
+    self.view.backgroundColor = .redColor()
+  }
+}
